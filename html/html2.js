c26dcec135bc1a51eae496299ada333bd9d9054a
沈志勇 <chinshiisamu@chenzhiyongdeMacBook-Pro.local>
2017-04-22 23:52:32 +0800

    more

--- html/html2.js
+++ html/html2.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 
 app.use('/cssFile', express.static(__dirname + '/assets'));
 
 app.get('/', function(req, res) {
 	res.sendFile('index.html', {root: path.join(__dirname, '../html')});
-})
+});
+
+app.get(/^(.+)$/, function(req ,resp){
+	console.log(req.params);
+	// if(fs.statSync(path.join(__dirname)))
+	resp.sendFile(req.params[0]+ '.html', {root: path.join(__dirname, '../html')});
+});
 
 app.listen(9090,function() {
 	console.log('Listening at Port 9090');
-})
+});
