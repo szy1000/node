616e693294c480b61baa6723f2de45c0144110fc
szy1000 <1538078053@qq.com>
2019-12-10 21:09:05 +0800

    add request

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-// var server = require("http");
-
-// server.createServer(engine).listen(9090);
-
-// function engine(request,response) {
-// 	response.writeHead(200,{'Content-Type':'text/plain'});
-// 	response.end('hey');
-// 	// console.log("q");
-// }
-
-var http = require("http");
-var server = http.createServer(engine);
-
-server.listen(9090, function(){
-	console.log("Hello");
-})
+const http = require("http");
+const queryString = require('querystring')
+const server = http.createServer(engine);
 
 function engine(request,response) {
+	const {method, url} = request
+	console.log(method)
+	console.log(url)
+	if(method === 'POST') {
+		console.log(request.headers)
+	}
+	const query = queryString.parse(url.split('?')[1])
+	console.log(query)
 	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end('hey'+ request.url);
-	// console.log("q");
+	response.end(
+		JSON.stringify(query) //不能直接返回对象
+	);
 }
+
+server.listen(9090, function(){
+	console.log("Listening in port 9090");
+})
