06b75c8b92e5823bc9f45515f0467912046e47b3
szy1000 <1538078053@qq.com>
2019-12-11 21:12:52 +0800

    combine get and post

--- server.js
+++ server.js
@@ -2,23 +2,36 @@ const http = require("http");
 const queryString = require('querystring')
 const server = http.createServer(engine);
 
-function engine(request,response) {
-	const { method, url, path } = request
+function engine(req,res) {
+	const { method, url } = req
+	const path = url.split('?')[0]
+	const query = queryString.parse(url.split('?')[1])
+
+	// res.setHeader('Content-type', 'text/html') // 返回html
+	res.setHeader('Content-type', 'application/json') //返回json
+
+	const data = {
+		method, url, path, query
+	}
+
 	if(method === 'POST') {
 		let temp = ''
-		request.on('data', chunk => {
+		req.on('data', chunk => {
 			temp += chunk.toString()
 		})
 
-		request.on('end', () => {
-			console.log(temp)
-			response.end('Hello POST')
+		req.on('end', () => {
+			data.temp = temp
+			res.end(JSON.stringify(data))
 		})
 	}
-	// const query = queryString.parse(url.split('?')[1])
-	// console.log('query', JSON.stringify(query))
-	// response.writeHead(200,{'Content-Type':'text/plain'});
-	// response.end(
+
+	if(method === 'GET') {
+		res.end(JSON.stringify(data))
+	}
+
+	// res.writeHead(200,{'Content-Type':'text/plain'});
+	// res.end(
 	// 	JSON.stringify('Hello GET') //不能直接返回对象
 	// );
 }
