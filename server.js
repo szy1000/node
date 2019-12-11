db9a878d36de2261be010a3747494467354cc4ac
szy1000 <1538078053@qq.com>
2019-12-11 20:57:15 +0800

    add post

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const queryString = require('querystring')
 const server = http.createServer(engine);
 
 function engine(request,response) {
-	const {method, url} = request
-	console.log(method)
-	console.log(url)
+	const { method, url, path } = request
 	if(method === 'POST') {
-		console.log(request.headers)
+		let temp = ''
+		request.on('data', chunk => {
+			temp += chunk.toString()
+		})
+
+		request.on('end', () => {
+			console.log(temp)
+			response.end('Hello POST')
+		})
 	}
-	const query = queryString.parse(url.split('?')[1])
-	console.log(query)
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end(
-		JSON.stringify(query) //不能直接返回对象
-	);
+	// const query = queryString.parse(url.split('?')[1])
+	// console.log('query', JSON.stringify(query))
+	// response.writeHead(200,{'Content-Type':'text/plain'});
+	// response.end(
+	// 	JSON.stringify('Hello GET') //不能直接返回对象
+	// );
 }
 
 server.listen(9090, function(){
